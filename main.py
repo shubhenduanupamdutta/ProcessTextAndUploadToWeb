a17486dcd6d2bcc1d82824f5a22e6f699312f10f
ShubhenduADutta <shubhenduanupam@gmail.com>
2023-09-23 14:03:37 +0530

    Add script for objective
    Added function to get files from directory (get_feedback_files), get feedback from the file(get_feedback) and post the feedback on url_endpoint(post_feedback)

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
 #!/usr/bin/env python3
 
+import os
+import requests
 
+relative_path_to_directory = "/data/feedback/"
+URL_ENDPOINT = "http://<corpweb-external-IP>/feedback/"
+
+
+def get_feedback_files():
+    """Returns a list of all files in the directory"""
+    return [relative_path_to_directory + str(name) for name in os.listdir(relative_path_to_directory)]
+
+
+def get_feedback(file):
+    """Returns a dictionary of the feedback data"""
+    feedback = {}
+    with open(file) as f:
+        feedback["title"] = f.readline().strip()
+        feedback["name"] = f.readline().strip()
+        feedback["date"] = f.readline().strip()
+        feedback["feedback"] = f.read().strip()
+
+    return feedback
+
+
+def post_feedback(feedback):
+    """Posts the feedback to the URL_ENDPOINT"""
+    response = requests.post(URL_ENDPOINT, json=feedback)
+    response.raise_for_status()
+
+
+def main():
+    """Main function"""
+    for file in get_feedback_files():
+        feedback = get_feedback(file)
+        post_feedback(feedback)
+
+
+if __name__ == '__main__':
+    main()
